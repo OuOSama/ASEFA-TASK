1849156ceb842a0a9214e26a3aa1e5ff76a89d0c
OuOSama <ouosamacute@gmail.com>
2024-10-27 18:13:08 +0700

    chore: update next.config.js with remotePatterns for use with openweathermap.org, coin-images.coingecko.com, and gasprice.kapook.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          hostname: 'openweathermap.org',
+        },
+        {
+          hostname: 'coin-images.coingecko.com',
+        },
+        {
+          hostname: 'gasprice.kapook.com',
+        },
+      ],
+    },
+  }
+  
+  export default nextConfig
+  
